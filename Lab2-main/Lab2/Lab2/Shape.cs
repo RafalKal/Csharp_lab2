b09116c1b01059f33be7a7bf23cc6f5d9f22ca79
rafal_ <83827739+RafalKal@users.noreply.github.com>
2022-01-04 23:24:53 +0100

    Add files via upload

---
+++ Lab2-main/Lab2/Lab2/Shape.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    abstract class Shape
+    {
+        int x = 0;
+        int y = 0;
+        int width = 0;
+        int height = 0;
+
+        public virtual void draw()
+        {
+
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Rysuje kwadrat");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Rysuje trójkąt");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Rysuje kółko");
+        }
+    }
+
+    class zad1
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> lista = new List<Shape>();
+            Circle c = new Circle();
+            Triangle t = new Triangle();
+            Rectangle r = new Rectangle();
+            lista.Add(c);
+            lista.Add(t);
+            lista.Add(r);
+
+            foreach(Shape shape in lista)
+            {
+                shape.draw();
+            }
+        }
+    }
+
+}
---
+++ Lab2-main/Zadanie2/Zadanie2/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System.Collections;
+
+abstract class Osoba
+{
+    private String Imie { get; set; }
+    private String Nazwisko { get; set; }
+    private String Pesel { get; set; }
+    private int Age;
+
+
+    public int getAge()
+    {
+        int year = Int32.Parse(Pesel[0].ToString()) * 10 + Int32.Parse(Pesel[1].ToString()) + ((Int32.Parse(Pesel[2].ToString()) / 2 + 1) % 5) * 100 + 1800;
+
+        return 2022 - year;
+
+    }
+    public String getGender()
+    {
+        if (Int32.Parse(Pesel.Substring(9, 1)) % 2 == 0)
+        {
+            return "k*bieta";
+        }
+        else
+        {
+            return "mężczyzna";
+        }
+    }
+    public virtual void GetEducationInfo()
+    {
+        System.Console.WriteLine("Uczy się");
+    }
+    public String GetFullName()
+    {
+        return Imie + " " + Nazwisko;
+    }
+    public virtual Boolean CanGoHomeAlone()
+    {
+        if (2022 - Age >= 12) return true;
+        return false;
+    }
+    public virtual void Info()
+    {
+
+    }
+}
+
+class Uczen : Osoba
+{
+    String Szkola { get; set; }
+    public void changeSchool(String szkola)
+    {
+        Szkola = szkola;
+    }
+    Boolean MozeSamWracacDoDomu { get; set; }
+    public override void Info()
+    {
+        System.Console.WriteLine("Nie może sam wracać poniżej 12 lat chyba że ma pozwolenie");
+    }
+
+}
+
+class Nauczyciel : Osoba
+{
+    private String TytulNaukowy { get; set; }
+    private List<Uczen> PodwladniUczniowie = new List<Uczen>();
+
+    public void WhichStudentCanGoHomeAlone(DateTime dateToCheck)
+    {
+        foreach (Uczen uczen in PodwladniUczniowie)
+        {
+            if (uczen.CanGoHomeAlone())
+            {
+                System.Console.WriteLine("Uczeń może wrócić sam do domu.");
+            }
+            else
+            {
+                uczen.Info();
+            }
+        }
+    }
+}
